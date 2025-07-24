919f35911f329392bbfb1d72115d86ef53a6450e
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2025-07-24 18:37:41 +0530

    star component

--- react/usePopcorn/src/main.jsx
+++ react/usePopcorn/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import Stars from "./Stars";
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <Stars color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} Stars</p>
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <Stars maxRating={10} />
+    <Stars
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <Stars size={24} color="red" className="test" defaultRating={3} />
+    <Test />
+  </StrictMode>
+);
