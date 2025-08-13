26753a75c5feb867a823ff4485c54ec792ac81b4
Bhavya Rohilla <117907248+BhavyaRohilla@users.noreply.github.com>
2025-08-13 17:12:10 +0530

    node-farm completed

--- Node_Old/1-node_farm/index.js
+++ Node_Old/1-node_farm/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 /*
 // Synchronous Way
 const data = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -27,8 +29,62 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
 */
 
 // server
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
-  res.end("hello from the server");
+  const { query, pathname: pathName } = url.parse(req.url, true);
+
+  // Overview
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
+  }
+  // Product Page
+  else if (pathName === "/product") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.end(output);
+  }
+  // API
+  else if (pathName === "/api") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(data);
+  }
+  // NOT FOUND
+  else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
