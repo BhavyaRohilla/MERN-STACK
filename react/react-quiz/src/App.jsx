587df6d216be9896bfb0d1978a2db43d6868c05d
Bhavya Rohilla <officialbhavyarohilla@gmail.com>
2025-08-25 19:30:44 +0530

    react-quiz-app

--- react/react-quiz/src/App.jsx
+++ react/react-quiz/src/App.jsx
@@ -1,9 +1,47 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+
+const initialState = {
+  questions: [],
+
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "DataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Active unknown");
+  }
+}
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 };
